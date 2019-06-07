4800ce60f9c9f2295df633291a2e38125ed63b32
bdhillon <bdhillon1994@gmail.com>
2019-06-06 21:06:53 -0700

    proper iso

--- scraper/index.js
+++ scraper/index.js
@@ -1,7 +1,8 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
-const options = {
+// Parameters
+const silver = {
     uri: `https://www.investing.com/commodities/silver-historical-data`,
     transform: function (body) {
       return cheerio.load(body);
@@ -9,20 +10,44 @@ const options = {
     headers: {
       'User-Agent': 'Request-Promise'
     }
-  };
+};
 
-rp(options)
-.then(($) => {
-$('#results_box').find('table.genTbl.closedTbl.historicalTbl tr').each(
-  function(index, element) {
-      textArray = $(element).text().split('\n');
-      if (index != 0)  {
-        let date = new Date(textArray[1]).toISOString().split("T")[0];
-        let price = textArray[2].replace(/ /g, '');
-        console.log("Date: "  + date + " Price: $" + price);
-      }
-  });
-})
-.catch((err) => {
-console.log(err);
+const gold = {
+  uri: `https://www.investing.com/commodities/gold-historical-data`,
+  transform: function (body) {
+    return cheerio.load(body);
+  },
+  headers: {
+    'User-Agent': 'Request-Promise'
+  }
+};
+
+// Methods
+toISOStr = (function(str) {
+  return new Date(new Date(new Date(str)
+            .toISOString().split("T")[0]) - 1)
+            .toISOString().split("T")[0];
 });
+
+fetchData = (function(options) {
+  rp(options)
+  .then(($) => {
+    $('#results_box').find('table.genTbl.closedTbl.historicalTbl tr').each(
+      function(index, element) {
+          textArray = $(element).text().split('\n');
+          if (index != 0)  {
+            let date = toISOStr(textArray[1]);
+            let price = textArray[2].replace(/ /g, '');
+            console.log("Date: "  + date + " Price: $" + price);
+          }
+      });
+  })
+  .catch((err) => {
+  console.log(err);
+  });
+});
+
+// Scrape data from URLs
+fetchData(silver);
+fetchData(gold);
+
